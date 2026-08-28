fcf9220efeaa8f26a74888ef93aa45f4292c5def
g-il263501-oss <g-il263501@dge.go.kr>
2026-08-28 15:04:31 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const star = document.createElement("div");
+
+star.innerHTML = "⭐";
+
+star.style.position = "fixed";
+star.style.left = "50%";
+star.style.top = "30%";
+star.style.fontSize = "25px";
+star.style.cursor = "pointer";
+
+document.body.appendChild(star);
